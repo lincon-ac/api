52e0443ce7bfaabd554649fb7e5f18c41d1333cd
Lincon Agrizzi <lincon_ac@hotmail.com>
2024-03-25 15:26:21 -0300

    bug

--- index.js
+++ index.js
@@ -35004,7 +35004,7 @@ app.get('/photos', (req, res) => {
         "title": "error quasi sunt cupiditate voluptate ea odit beatae",
         "url": "https://via.placeholder.com/600/6dd9cb",
         "thumbnailUrl": "https://via.placeholder.com/150/6dd9cb"
-    }
+   }
 ]
     
     res.json(photos);
